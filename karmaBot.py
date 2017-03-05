b81c2c97ffa5454307315f513c96a278bdc7d322
Nathan Contino <ncontino@u.rochester.edu>
2017-03-05 17:10:32 -0500

    Refactor
    Slightly improved code quality.

--- karmaBot.py
+++ karmaBot.py
@@ -34,15 +34,16 @@ def read_id_info():
 def handle(msg):
 	content_type, chat_type, chat_id = telepot.glance(msg)
 	sender = msg['from']
+	sender_id = str(sender['id']);
 	receiver = '';
 	if str(sender['id']) not in id_count:
 		id_nicks[str(sender['id'])] = []
 		if 'username' in sender:
-			id_nicks[str(sender['id'])].append(sender['username'].lower())
+			id_nicks[sender_id].append(sender['username'].lower())
 		if 'first_name' in sender:
-			id_nicks[str(sender['id'])].append(sender['first_name'].lower())
+			id_nicks[sender_id].append(sender['first_name'].lower())
 		if 'last_name' in sender:
-			id_nicks[str(sender['id'])].append(sender['last_name'].lower())
+			id_nicks[sender_id].append(sender['last_name'].lower())
 		id_count[str(sender['id'])] = 0;
 		write_id_info()
 	if content_type == 'text':
@@ -50,20 +51,24 @@ def handle(msg):
 			conflict = False;
 			msg_words = msg['text'].split()
 			if len(msg_words) == 3:
-				for id in id_nicks:
-					if msg_words[2] in id:
-						conflict = True;
-						bot.sendMessage(chat_id, 'Bad user. Don\'t steal usernames.')
+				if msg_words[2] in id_nicks[sender_id]:
+					swap_index = id_nicks[sender_id].index(msg_words[2])
+					temp_nick = id_nicks[sender_id][0]
+					id_nicks[sender_id][0] = id_nicks[sender_id][swap_index]
+					id_nicks[sender_id][swap_index] = temp_nick
+					bot.sendMessage(chat_id, 'Successfully changed primary nickname to existing nickname ' + msg_words[2])
+				else:
+					for id in id_nicks:
+						for nick in id_nicks[id]:
+							if nick.lower() == msg_words[2].lower():
+								conflict = True;
+								bot.sendMessage(chat_id, 'Bad user. Don\'t steal usernames.')
+								break;
 				if not conflict:
-					if msg_words[2] not in id_nicks[str(sender['id'])]:
-						rename(str(sender['id']), msg_words[2]) 
-					else:
-						swap_index = id_nicks[str(sender['id'])].index(msg_words[2])
-						temp_nick = id_nicks[str(sender['id'])][0]
-						id_nicks[str(sender['id'])][0] = id_nicks[str(sender['id'])][swap_index]
-						id_nicks[str(sender['id'])][swap_index] = temp_nick
-					write_id_info()
-					bot.sendMessage(chat_id, 'Successfully renamed to ' + msg_words[2])
+					if msg_words[2] not in id_nicks[sender_id]:
+						rename(sender_id, msg_words[2])
+						bot.sendMessage(chat_id, 'Successfully renamed to ' + msg_words[2])
+				write_id_info()
 		elif '++' in msg['text']:
 			count_handler(1 , msg['text'], sender['id'], chat_id)
 		elif '--' in msg['text']:
@@ -73,6 +78,16 @@ def handle(msg):
 			for user in id_count:
 				count_pls += id_nicks[user][0] + ' = ' + str(id_count[user]) + '\n'
 			bot.sendMessage(chat_id, count_pls)
+		elif '+=' in msg['text']:
+			bot.sendMessage(chat_id, 'Unsupported behaviour. Pls increment by one brownie point at a time.')
+		elif '-=' in msg['text']:
+			bot.sendMessage(chat_id, 'Unsupported behaviour. Pls decrement by one brownie point at a time.')
+		elif '*=' in msg['text']:
+			bot.sendMessage(chat_id, 'Unsupported behaviour. Pls increment by one brownie point at a time.')
+		elif '/=' in msg['text']:
+			bot.sendMessage(chat_id, 'Unsupported behaviour. Pls decrement by one brownie point at a time.')
+		else:
+			bot.sendMessage(chat_id, 'I didn\'t understand that. Please consult the docs at https://github.com/nathan-contino/infamyBot')
 				
 
 def count_handler(direction, text, sender_id, chat_id):
@@ -103,7 +118,8 @@ def count_handler(direction, text, sender_id, chat_id):
 					id_count[id] = str(count)
 					bot.sendMessage(chat_id, 'Updated ' + str(last_of_first) + '\'s score')
 					write_id_info()
-					return last_of_first; 
+				else:
+					bot.sendMessage(chat_id, 'Bad user. Don\'t try to change your own score.')
 
 def rename(sender_id, new_name):
 	id_nicks[sender_id].insert(0, new_name);
