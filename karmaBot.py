e2897a8c93b14f144a2ac787db73004eea621440
Nathan Contino <ncontino@u.rochester.edu>
2017-03-05 15:44:00 -0500

    Finally (somewhat) functional
    Vaguely useful, though uses a global (not chat-specific) text file (not
    database!) of users. Will add sqlite and chat-specific counts in a future
    update.

--- karmaBot.py
+++ karmaBot.py
@@ -6,14 +6,16 @@ id_nicks = {};
 bot = telepot.Bot('261334375:AAHlnUWwZ8cxTEu4JSJ90PcMBP__8V9IFiw');
 
 def write_id_info():
+	open('ids.txt', 'a').close()
 	f = open('ids.txt', 'w')
-	
+	buffer = ''	
 	for id in id_count:
-		f.write(str(id) + ' ')
-		f.write(str(id_count[id]) + ' ')
+		buffer += str(id) + ' '
+		buffer += str(id_count[id]) + ' '
 		for nick in id_nicks[id]:
-			f.write(nick + ' ') 
-		f.write('\n')
+			buffer += nick + ' ' 
+		buffer += '\n'
+	f.write(buffer)
 	f.close()
 
 def read_id_info():
@@ -32,28 +34,36 @@ def read_id_info():
 def handle(msg):
 	content_type, chat_type, chat_id = telepot.glance(msg)
 	sender = msg['from']
-#	print(content_type, chat_type, chat_id)
-#	print(msg)
 	receiver = '';
-	if sender['id'] not in id_count:
-		id_nicks[sender['id']] = []
+	if str(sender['id']) not in id_count:
+		id_nicks[str(sender['id'])] = []
 		if 'username' in sender:
-			id_nicks[sender['id']].append(sender['username'].lower())
+			id_nicks[str(sender['id'])].append(sender['username'].lower())
 		if 'first_name' in sender:
-			id_nicks[sender['id']].append(sender['first_name'].lower())
+			id_nicks[str(sender['id'])].append(sender['first_name'].lower())
 		if 'last_name' in sender:
-			id_nicks[sender['id']].append(sender['last_name'].lower())
-		id_count[sender['id']] = 0;
+			id_nicks[str(sender['id'])].append(sender['last_name'].lower())
+		id_count[str(sender['id'])] = 0;
 		write_id_info()
 	if content_type == 'text':
-		if '++' in msg['text']:
+		if 'rename' in msg['text']:
+			msg_words = msg['text'].split()
+			if len(msg_words) == 3:
+				if msg_words[2] not in id_nicks[str(sender['id'])]:
+					rename(str(sender['id']), msg_words[2]) 
+				else:
+					swap_index = id_nicks[str(sender['id'])].index(msg_words[2])
+					temp_nick = id_nicks[str(sender['id'])][0]
+					id_nicks[str(sender['id'])][0] = id_nicks[str(sender['id'])][swap_index]
+					id_nicks[str(sender['id'])][swap_index] = temp_nick
+		elif '++' in msg['text']:
 			count_handler(1 , msg['text'], sender['id'], chat_id)
-		if '--' in msg['text']:
+		elif '--' in msg['text']:
 			count_handler(-1 , msg['text'], sender['id'], chat_id)
-		if 'count pls' in msg['text']:
+		elif 'count pls' in msg['text']:
 			count_pls = 'Current counts:\n'
 			for user in id_count:
-				count_pls += id_nicks[user][2] + ' = ' + str(id_count[user]) + '\n'
+				count_pls += id_nicks[user][0] + ' = ' + str(id_count[user]) + '\n'
 			bot.sendMessage(chat_id, count_pls)
 				
 
@@ -66,15 +76,29 @@ def count_handler(direction, text, sender_id, chat_id):
 	halves = text.split(crtyp)
 	first = halves[0].split()
 	last_of_first = first[len(first)-1].lower()
-	for id in id_nicks:
-		if last_of_first in id_nicks[id]:
-			if int(id) != sender_id:
-				count = int(id_count[id])
-				count += direction;
-				id_count[id] = str(count)
-				bot.sendMessage(chat_id, 'Updated ' + str(last_of_first) + '\'s score')
-				write_id_info()
-				return last_of_first; 
+	if last_of_first == 'bot':
+		id_nicks['0'] = ['bot']
+		if '0' in id_count:
+			count = int(id_count['0'])
+			count += direction;
+			id_count['0'] = str(count)
+		else:
+			id_count['0'] = 0;
+			id_nicks['0'] = ['bot']
+		bot.sendMessage(chat_id, 'Updated bot\'s score')
+	else :
+		for id in id_nicks:
+			if last_of_first in id_nicks[id]:
+				if int(id) != sender_id:
+					count = int(id_count[id])
+					count += direction;
+					id_count[id] = str(count)
+					bot.sendMessage(chat_id, 'Updated ' + str(last_of_first) + '\'s score')
+					write_id_info()
+					return last_of_first; 
+
+def rename(sender_id, new_name):
+	id_nicks[sender_id].insert(0, new_name);
 
 def main():
 	bot.message_loop(handle)
